7f8ce85626c2baa2bec4e8a81df23a49c300e672
Muhammad Sirojul Fuad <94115640+Sirojulf@users.noreply.github.com>
2024-12-05 20:28:43 +0700

    Update Main.go

--- Main.go
+++ Main.go
@@ -13,9 +13,9 @@ type Kelompok3 struct {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Acces-Control-Allow-Origin", "*")
-		w.Header().Set("Acces-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Acces-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
